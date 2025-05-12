3dce7a474ee76d5c70dc121ea235261e55278d63
Usuario <Usuario@DAVID>
2025-05-12 19:47:19 +0200

    Añadido Tablero de ejemplo

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ def index():
         path = 'static/uploaded_sudoku.jpg'
         img_file.save(path)
 
+        # Tablero de ejemplo
+        # board = np.array([
+        #     [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        #     [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        #     [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        #     [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        #     [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        #     [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        #     [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        #     [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        #     [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        # ])
+
         board = procesar_imagen_a_tablero(path)  # Procesar imagen a tablero
         original_board = [row.tolist() for row in board]  # Convertir para renderizar
 
